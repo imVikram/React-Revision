15ae2f0561156ec18b7d04839085a354d5585edb
Vikram Singh <Vikram.Singh@inn-flow.com>
2023-09-11 00:27:25 +0530

    first app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>My react App | Vikram Singh</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>My Vite + React App</h1>
+    </>
+  )
+}
+
+export default App
